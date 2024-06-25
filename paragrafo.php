3a81b56f5c223d58c4778f05011f07ddfc454779
DanieleTrippodo <trippododaniele@gmail.com>
2024-06-25 17:40:37 +0200

    complete logic PHP

--- paragrafo.php
+++ paragrafo.php
@@ -1,7 +1,7 @@
 <?php
 
 /* Variabile che contiene il paragrafo (non censurato) */
-$Paragrafo = "";
+$Paragrafo = "Questo è un Paragrafo di PROVA";
 
 
 /* Stampiamo il paragrafo originale e anche la sua lunghezza */
@@ -12,5 +12,17 @@ echo "<p>Lunghezza: " . strlen($Paragrafo) . "</p>";
 
 /* Prendi la parola da censurare nell'HTML */
 $censored_word = $_GET['censura'];
+
+/* --------------------------------------------------------------------------------- */
+
+// Censura la parola nel paragrafo
+$censored_Paragrafo = str_replace($censored_word, '***', $Paragrafo);
+
+
+// Stampa il paragrafo censurato e la sua lunghezza
+echo "<h2>Paragrafo Censurato:</h2>";
+echo "<p>$censored_paragraph</p>";
+echo "<p>Lunghezza: " . strlen($censored_paragraph) . "</p>";
+
 
 ?>
